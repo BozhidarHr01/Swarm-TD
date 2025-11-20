386510d7c1bfb04651a2c8d3ab27f4b48ccd4cc8
BozhidarHr01 <bozhidar.hristov01@gmail.com>
2025-11-20 17:32:23 +0200

    added tmx dependancies

--- maps/tsx/walls_floor.tsx
+++ maps/tsx/walls_floor.tsx
@@ -2,8 +2,8 @@
 <tileset version="1.10" tiledversion="1.11.2" name="walls_floor" tilewidth="16" tileheight="16" tilecount="299" columns="13">
  <image source="../../../craftpix-net-169442-free-2d-top-down-pixel-dungeon-asset-pack/PNG/walls_floor.png" width="208" height="368"/>
  <tile id="79" probability="0.95"/>
- <tile id="284" probability="0.05"/>
- <tile id="285" probability="0.05"/>
- <tile id="297" probability="0.05"/>
- <tile id="298" probability="0.05"/>
+ <tile id="284" probability="0.01"/>
+ <tile id="285" probability="0.01"/>
+ <tile id="297" probability="0.01"/>
+ <tile id="298" probability="0.01"/>
 </tileset>
